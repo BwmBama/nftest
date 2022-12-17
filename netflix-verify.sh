599845a317ea667bc84d15497b96c63f2883ace2
root <root@debian.debian>
2022-12-17 13:39:00 +0800

    2022-12-17

--- netflix-verify.sh
+++ netflix-verify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-    curl   --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" > ./1.txt
+    curl   --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/70143836" > ./1.txt
 git add --all
 if [ $# -eq 0 ]
 then
